1ab51fcb452952be2a2b9db1bab4f32cab8b0d82
Gavin Kariuki <gavin.kariuki@student.moringaschool.com>
2021-10-11 21:08:59 +0300

    declare the needed variables

--- src/app/cards/cards.component.ts
+++ src/app/cards/cards.component.ts
@@ -1,15 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import {
+  faThumbsUp,
+  faThumbsDown,
+  faTrash,
+} from '@fortawesome/free-solid-svg-icons';
+import { DisplayQuotes } from 'src/displayQuotes';
 
 @Component({
   selector: 'app-cards',
   templateUrl: './cards.component.html',
-  styleUrls: ['./cards.component.css']
+  styleUrls: ['./cards.component.css'],
 })
 export class CardsComponent implements OnInit {
+  name: string;
+  yourName: string;
+  em: string;
+  email: string;
+  quote: string;
+  jina: string;
+  publishedDate: Date;
+  newQuote: any;
+  like = faThumbsUp;
+  downvote = faThumbsDown;
+  delete = faTrash;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
