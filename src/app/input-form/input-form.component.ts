6299c65f8b23ac33c562c8f130414199a7ad42c7
Gavin Kariuki <gavin.kariuki@student.moringaschool.com>
2021-10-11 21:05:23 +0300

    import ts file for displaying quotes into form ts

--- src/app/input-form/input-form.component.ts
+++ src/app/input-form/input-form.component.ts
@@ -1,10 +1,11 @@
+import { DisplayQuotes } from './../../displayQuotes';
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import {
   faThumbsUp,
   faThumbsDown,
   faTrash,
 } from '@fortawesome/free-solid-svg-icons';
-import { DisplayQuotes } from 'displayQuotes';
+
 
 @Component({
   selector: 'app-input-form',
@@ -25,8 +26,34 @@ export class InputFormComponent implements OnInit {
   delete = faTrash;
 
   @Output() postQuotes = new EventEmitter();
+  disQuotes = [
+    new DisplayQuotes(
+      0,
+      'Gavin Kariuki',
+      'gavin.kariuki@student.moringaschool.com',
+      'Big Chungus',
+      0,
+      0,
+      new Date()
+    ),
+  ];
 
+  removeCard(i) {
+    this.disQuotes.splice(i, 1);
+  }
   constructor() {}
+
+  onSubmit() {
+    this.newQuote = new DisplayQuotes(
+      0,
+      this.jina,
+      this.email,
+      this.quote,
+      0,
+      0,
+      new Date()
+    );
+  }
 
   ngOnInit(): void {}
 }
