48e8fe1a1193ee5bc5d5491c07aad14d5b546855
Gavin Kariuki <gavin.kariuki@student.moringaschool.com>
2021-10-11 19:39:55 +0300

    add a bootstrap card

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { InputFormComponent } from './input-form/input-form.component';
+import { CardsComponent } from './cards/cards.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    InputFormComponent
+    InputFormComponent,
+    CardsComponent
   ],
   imports: [
     BrowserModule,
