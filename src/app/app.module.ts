57b6cd211e7e185968d091a4e92df98e8cb08a18
Gavin Kariuki <gavin.kariuki@student.moringaschool.com>
2021-10-11 20:54:07 +0300

    import all the required modules for the app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -7,16 +9,9 @@ import { InputFormComponent } from './input-form/input-form.component';
 import { CardsComponent } from './cards/cards.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    InputFormComponent,
-    CardsComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, InputFormComponent, CardsComponent],
+  imports: [BrowserModule, AppRoutingModule, FormsModule, FontAwesomeModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
