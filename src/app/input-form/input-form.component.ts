8c32f508e06aa0e70cf41e77e27c1d40a60e7818
Gavin Kariuki <gavin.kariuki@student.moringaschool.com>
2021-10-11 21:02:02 +0300

    declare all variables

--- src/app/input-form/input-form.component.ts
+++ src/app/input-form/input-form.component.ts
@@ -6,14 +6,23 @@ import {
 } from '@fortawesome/free-solid-svg-icons';
 import { DisplayQuotes } from 'displayQuotes';
 
-
-
 @Component({
   selector: 'app-input-form',
   templateUrl: './input-form.component.html',
   styleUrls: ['./input-form.component.css'],
 })
 export class InputFormComponent implements OnInit {
+  name: string;
+  yourName: string;
+  em: string;
+  email: string;
+  quote: string;
+  jina: string;
+  publishedDate: Date;
+  newQuote: any;
+  like = faThumbsUp;
+  downvote = faThumbsDown;
+  delete = faTrash;
   constructor() {}
 
   ngOnInit(): void {}
